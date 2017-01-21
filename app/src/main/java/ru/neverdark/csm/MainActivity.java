34aa8570625878d51be072f94e5783cf1c4ea57e
Artem Yankovskiy <artemyankovskiy@gmail.com>
2017-01-21 22:18:08 +1000

    Возможность отправки базы данных на почту

--- app/src/main/java/ru/neverdark/csm/MainActivity.java
+++ app/src/main/java/ru/neverdark/csm/MainActivity.java
@@ -1,11 +1,19 @@
 package ru.neverdark.csm;
 
+import android.Manifest;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,10 +23,17 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.List;
+import java.util.Locale;
 
 import ru.neverdark.csm.components.TrackerService;
 import ru.neverdark.csm.fragments.MainFragment;
@@ -30,6 +45,7 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, MainFragment.OnFragmentInteractionListener {
 
     public static final String TAG = "MainActivity";
+    private static final int PERMISSION_REQUEST_BACKUP_DATABASE = 1;
 
     private MainFragment mMainFragment;
     private ActionBarDrawerToggle mToggle;
@@ -136,10 +152,50 @@ public class MainActivity extends AppCompatActivity
 
             } else if (id == R.id.nav_send) {
 
+            } else if (id == R.id.nav_backup_db) {
+                backupDatabase();
             }
         }
         mDrawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void backupDatabase() {
+        if (Utils.isExternalStorageWritable()) {
+            if (checkAndRequirePermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, PERMISSION_REQUEST_BACKUP_DATABASE)) {
+                new BackupDBTask().execute("gpsdata");
+            }
+        } else {
+            Toast.makeText(this, R.string.external_storage_not_available, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_BACKUP_DATABASE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                backupDatabase();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    /**
+     * Проверяет наличие прав и в случае необходимости запрашивает их
+     *
+     * @param permission  права доступа для проверки и запроса разрешения
+     * @param requestCode код запроса прав доступа для обработки onRequestPermissionsResult
+     * @return true если права доступа есть
+     */
+    private boolean checkAndRequirePermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
+            return false;
+        }
     }
 
     private void setDrawerDisabledState(boolean isDisabled) {
@@ -221,6 +277,64 @@ public class MainActivity extends AppCompatActivity
         if (mBound) {
             unbindService(mConnection);
             mBound = false;
+        }
+    }
+
+    private class BackupDBTask extends AsyncTask<String, Void, Boolean> {
+
+        private File mBackupDbFile;
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            return exportDatabse(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean status) {
+            if (status) {
+                if (mBackupDbFile.exists()) {
+                    Uri path = Uri.fromFile(mBackupDbFile);
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    String to[] = {"artemyankovskiy@gmail.com"};
+                    intent.setType("application/x-sqlite3");
+                    intent.putExtra(Intent.EXTRA_EMAIL, to);
+                    intent.putExtra(Intent.EXTRA_STREAM, path);
+                    intent.putExtra(Intent.EXTRA_SUBJECT, "Database for analyse");
+                    startActivity(Intent.createChooser(intent, "Send email..."));
+                } else {
+                    status = false;
+                }
+            }
+
+            if (!status) {
+                Toast.makeText(MainActivity.this, R.string.backup_database_fail, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        private boolean exportDatabse(String databaseName) {
+            boolean status = false;
+            try {
+                String backupDBPath = "logs.db";
+                File currentDB = getDatabasePath(databaseName);
+                mBackupDbFile = new File(getExternalFilesDir(null), backupDBPath);
+
+                if (currentDB.exists()) {
+                    if (mBackupDbFile.exists()) {
+                        mBackupDbFile.delete();
+                    }
+                    FileChannel src = new FileInputStream(currentDB).getChannel();
+                    FileChannel dst = new FileOutputStream(mBackupDbFile).getChannel();
+                    dst.transferFrom(src, 0, src.size());
+                    src.close();
+                    dst.close();
+                    status = true;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return status;
         }
     }
 }
